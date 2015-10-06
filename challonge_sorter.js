81c49a68943af6aad804216bfc3f784c1dba0409
Shawn Krisman <shawn@everlane.com>
2015-10-05 23:45:12 -0700

    now have information about mutation of the sortable list.

--- challonge_sorter.js
+++ challonge_sorter.js
@@ -1,1 +1,25 @@
-console.log("i'm here!");
+$(function() {
+  console.log("i'm here!");
+
+  // configuration of the observer:
+  var config = { attributes: true, childList: true, characterData: true, subtree: true };
+
+
+  $('#new_participant').submit(function() {
+    console.log('clicked motherfucker');
+
+    var sortableSelector = 'ol#participants.participant-list.editable.sortable.ui-sortable';
+    var sortable = $(sortableSelector);
+
+    var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        console.log(mutation.type);
+      });
+    });
+
+
+    // pass in the target node, as well as the observer options
+    observer.observe(sortable[0], config);
+    sortable.off();
+  })
+});
