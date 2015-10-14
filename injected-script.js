c87dd735afa6f7ec6106e4c7fc9a355d516092c2
Shawn Krisman <shawn@everlane.com>
2015-10-14 02:07:12 -0700

    player scores are added initially.

--- injected-script.js
+++ injected-script.js
@@ -24,10 +24,44 @@
     pma.addInitializer(function() {
       var collection = this.subView.collection;
       collectionSize = collection.size();
+      populateRankingStatistics(collection);
       collection.on("reset add remove",
         onCollectionChange,
         {collection: collection}
       );
+    });
+  };
+
+  var getNames = function(collection) {
+    return collection.models.map(function(model) {
+      return model.get('display_name');
+    })
+  };
+
+  var getPlayerData = function(name, data) {
+    return data.find(function(elem) {
+      return elem.name === name;
+    });
+  };
+
+  var populateRankingStatistics = function(collection) {
+    sortPlayersRequest(collection).done(function(playerDatum) {
+      var names = getNames(collection);
+      names.forEach(function(name) {
+        var elem = $('.participant-model [title="' + name + '"]');
+        var playerData = getPlayerData(name, playerDatum);
+        elem.after('<span style="position:absolute;left:370px">' +
+                   playerData.min.toFixed() + ' &mdash; ' +
+                   playerData.max.toFixed() + '</span>');
+      });
+    })
+  };
+
+
+  var sortPlayersRequest = function(collection) {
+    var names = getNames(collection);
+    return $.getJSON('http://smashmine.com/sort', {
+      players: JSON.stringify(names)
     });
   };
 
